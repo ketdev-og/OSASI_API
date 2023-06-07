d8780a8b0e8f27dfc87217c51f551919434ac01e
ketem <chylau12@gmail.com>
2023-06-07 21:44:23 +0100

    get single restaurant

--- src/main/java/com/project/osasi/utils/constants/Urls.java
+++ src/main/java/com/project/osasi/utils/constants/Urls.java
@@ -1,5 +1,6 @@
 package com.project.osasi.utils.constants;
 
 public class Urls {
-    public static final String GET_RESTURANT_BY_CITYNAME = "/restaurant/city";
+    public static final String SEARCH_REST_BY_CITYNAME = "/restaurant/city";
+    public static final String GET_REST = "/restaurant/{id}";
 }
