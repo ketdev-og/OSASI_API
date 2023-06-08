e18c8915aabb1c166c63d53f35bbc00d9f7859fc
ketem <chylau12@gmail.com>
2023-06-08 14:46:00 +0100

    cors config

---
+++ src/main/java/com/project/osasi/configurations/CorsConfigurer.java
@@ -0,0 +1,22 @@
+package com.project.osasi.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfigurer {
+
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
